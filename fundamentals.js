f364d89cef5d7e95c74e695c2a86f1766fecd269
Amarinder Pandher <amarinder_pandher@yahoo.com>
2017-08-18 09:16:53 -0400

    finished all 6 questions

--- fundamentals.js
+++ fundamentals.js
@@ -2,26 +2,34 @@
 
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
+var colors = ["red", "blue", "green"];
 
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = colors[2];
 
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
-
+var favoriteColors = [];
 
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
-
+for (var i = 0; i < colors.length; i++){
+  favoriteColors.push(colors[i]);
+}
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+var instructor = {
+  male: true,
+  age: 24,
+  eyes: 'brown'
+};
 
 
 
@@ -29,3 +37,4 @@
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor['facial-hair'] = true;
